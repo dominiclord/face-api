9b65af4d4e247a2dae7e82d4c13c905ea32a3c0c
dominiclord <dlord@outlook.com>
2016-06-18 11:25:45 -0400

    Return a single face from multiple results

--- src/Faces/Action/Face/ApiAction.php
+++ src/Faces/Action/Face/ApiAction.php
@@ -108,10 +108,15 @@ class ApiAction extends AbstractAction {
 		$attachments = [];
 		foreach ($faces as $face) {
 			$attachments[] = [
-                'fallback' => 'This is a face.',
+                'fallback' => 'This is a face.', // @todo Change to something like "USER has sent a face."
 				'image_url' => $this->baseUrl() . $face->image()
 			];
 		}
+
+        // Pick a random face
+        if (count($faces) > 1) {
+            $attachments = $attachments[rand(0, count($faces) - 1)];
+        }
 
         $text = (count($faces) > 0) ? '[user] is feeling ' . $this->emotion() . '.' : 'Couldn\'t find a face for that emotion. Maybe you should create it yourself. Or go back to work. Thanks.';
         $responseType = (count($faces) > 0) ? 'in_channel' : 'ephemeral';
